2d75778b42e55155d5b23e73f099e28847b8db3b
miskens@hotmail.com <miskens@hotmail.com>
2015-12-18 13:07:39 +0100

    Initial commit of created MVC project to git

---
+++ Miskens/Miskens/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Miskens.Startup))]
+namespace Miskens
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
